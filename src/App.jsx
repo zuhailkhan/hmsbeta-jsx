ced23daa36dce57e3dd28ad6c472acd714f37f4e
zuhailkhan <zuhail.khan@appstone.in>
2023-02-02 17:54:02 +0530

    Destructured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useRoutes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation, useNavigate, useRoutes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import SideBar from './components/SideBar'
+import useAuth from './hooks/useAuth'
 import routes from './routes'
+import { publicRoutes } from './routes'
 
 function App() {
   const element = useRoutes(routes)
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { auth } = useAuth()
   
+  useEffect(()=>{
+    let fi = publicRoutes.find(x => x.path == location.pathname)
+    if(!fi){
+      if(!auth || !auth?.status) {
+        alert('unauthorized')
+        navigate('/login')
+      }
+    }
+  },[location])
+
   return (
     <>
       {/* NavigationBar */}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,19 @@
 import Home from '../views/Home';
 import Login from '../views/Login';
 import Signup from '../views/Signup';
-
-const routes = [
+  
+export const routes = [
     {
         path: '/',
         element: <Home />
     },
+    {
+        path: '/*',
+        element: <Home />
+    }
+]
+
+export const publicRoutes = [
     {
         path: '/login',
         element: <Login />
@@ -15,10 +22,6 @@ const routes = [
         path: '/signup',
         element: <Signup />
     },
-    {
-        path: '/*',
-        element: <Home />,
-    }
 ]
 
-export default routes
+export default [...routes, ...publicRoutes]
