bd5592c51ebacb164de355a5e4b405afc08c5de0
zuhailkhan <zuhail.khan@appstone.in>
2023-02-02 17:54:23 +0530

    Navbar and Sidebar ui & related changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 function NavBar() {
+  const { auth, setAuth } = useAuth()
+  const navigate = useNavigate()
+
+  const logoutHandler = () => {
+    setAuth({})
+    navigate('/login')
+    return
+  }
+
   return (
     <div className="sticky top-0 w-screen bg-slate-500 p-3 text-white">
         <div className="flex justify-between mx-3">
-            <p>Logo</p>
-            <div className="flex gap-5">
-              <NavLink to='/login'>Login</NavLink>
-              <NavLink to='/signup'>Sign up</NavLink>
-            </div>
+            <NavLink to="/">Logo</NavLink>
+            {
+              auth && auth.status && 
+              <div className='px-3'>
+                <button onClick={logoutHandler}>Logout</button>
+              </div>
+            }
+
+            {
+              (!auth || !auth.status) && 
+              <div className="flex gap-5">
+                <NavLink to='/login'>Login</NavLink>
+                <NavLink to='/signup'>Sign up</NavLink>
+              </div> 
+             }
         </div>
     </div>
   )
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from 'react-router-dom'
 function SideBar() {
   return (
-    <div className="h-screen flex flex-col gap-5">
-        <NavLink to='/' className="h-1/4 bg-blue-300">Dashboard</NavLink>
-        <NavLink to='/complaints' className="h-1/4 bg-blue-300">Complaints</NavLink>
-        <NavLink to='/profile' className="h-1/4 bg-blue-300">Profile</NavLink>
-        <NavLink to='/history' className="h-1/4 bg-blue-300">History</NavLink>
+    <div className="h-screen flex flex-col gap-1">
+        <NavLink to='/' className="h-10 bg-blue-300">Dashboard</NavLink>
+        <NavLink to='/complaints' className="h-10 bg-blue-300">Complaints</NavLink>
+        <NavLink to='/profile' className="h-10 bg-blue-300">Profile</NavLink>
+        <NavLink to='/history' className="h-10 bg-blue-300">History</NavLink>
     </div>
   )
 }
